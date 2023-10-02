fecd39eb744c2a949db3586ebebaa3c0452fad87
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2023-10-03 01:24:55 +0700

    add

--- src/main/java/com/meenrock/demo/api/controller/MyController.java
+++ src/main/java/com/meenrock/demo/api/controller/MyController.java
@@ -1,8 +1,15 @@
 package com.meenrock.demo.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.meenrock.demo.api.service.DatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class MyController {
 
+    private final DatabaseService database_service;
+    @Autowired
+    public MyController(DatabaseService databaseService) {
+        this.database_service = databaseService;
+    }
 }
